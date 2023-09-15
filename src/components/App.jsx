0f811c2c0a68d195083b6b18a16c6afcd58134b5
okuzmychov <o.kuzmichev@gmail.com>
2023-09-15 21:58:02 +0300

    update-04

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,94 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { GlobalStyle } from 'GlobalStyle';
+import { Layout } from 'Layout';
+import { Component } from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export class App extends Component {
+  state = {
+  contacts: [],
+    filter: '',
+  };
+
+    componentDidMount() {
+    const savedContscts = localStorage.getItem('contacts');
+    if (savedContscts !== null) {
+      this.setState({ contacts: JSON.parse(savedContscts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  onSubmitForm = (newContact) => {
+    const isDublicate = this.state.contacts.some(
+      contact =>
+        contact.name.toLowerCase() === newContact.name.toLowerCase() ||
+        contact.number === newContact.number
+    );
+
+    if (isDublicate) {
+      toast.error(
+        "Контакт з таким ім'ям або номером вже є в списку!",
+        {
+          position: 'top-center',
+          autoClose: 2700,
+          theme: 'colored',
+        }
+      );
+      return;
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+    }
+  };
+
+ handleFiltrChange = value => {
+    this.setState({ filter: value });
+  };
+
+  filterContacts = (contacts, filter) => {
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  };
+
+  handleDeleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = this.filterContacts(contacts, filter);
+    return (
+      <Layout>
+      <Section title={'Phonebook'}>
+        <ContactForm onSubmit={this.onSubmitForm} />
+        </Section>
+        <Section title={'Contacts'}>
+          <Filter value={filter} onFilterChange={this.handleFiltrChange} />
+
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.handleDeleteContact}
+        />
+        </Section>
+        
+        <ToastContainer />
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { ThemeProvider } from 'styled-components';
 import './index.css';
+
+const theme = {
+  colors: {
+    lightgreen: '#A3B18A',
+    green: '#3A5A40',
+    white: '#FFFFFF',
+    beige: '#DAD7CD',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
